72025e5e7e35d4c6c515efa7b13c0d19405742cf
Donna Seidel <dseidel01@gmail.com>
2021-08-30 22:55:21 -0500

    Add CatTests

---
+++ CatTests/HouseCatTests.cs
@@ -0,0 +1,16 @@
+﻿using Cats;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CatTests
+{
+    [TestClass]
+    public class HouseCatTests
+    {
+        [TestMethod]
+        public void InheritsBaseInFirstConstructor()
+        {
+            HouseCat keyboardCat = new HouseCat("Keyboard Cat", 7);
+            Assert.AreEqual(7, keyboardCat.Weight, .001);
+        }
+    }
+}
---
+++ CatTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CatTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
